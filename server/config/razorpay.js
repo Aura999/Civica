de835604f2d2f81cd7cc8b6046489f9ef41641eb
Aura999 <satyam2002maanu@gmail.com>
2025-07-03 07:59:30 +0530

    Updated something

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -6,3 +6,6 @@ exports.instance = new Razorpay({
 	key_id: process.env.RAZORPAY_KEY,
 	key_secret: process.env.RAZORPAY_SECRET,
 });
+
+console.log("razpay key --> ",process.env.RAZORPAY_KEY)
+console.log("razpay secret --> ",process.env.RAZORPAY_SECRET)
