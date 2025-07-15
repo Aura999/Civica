29a635d12ffc0da55d8f858565b53ed45a77de5a
Satyam Mishra <satyam2002maanu@gmail.com>
2025-07-15 09:55:41 +0530

    bug updates

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -1,3 +1,5 @@
+//myprofile.js
+
 import { RiEditBoxLine } from "react-icons/ri"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
@@ -14,7 +16,7 @@ export default function MyProfile() {
       <h1 className="mb-14 text-3xl font-medium text-richblack-5">
         My Profile
       </h1>
-      <div className="flex items-center justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
+      <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-4 sm:px-12">
         <div className="flex items-center gap-x-4">
           <img
             src={user?.image}
