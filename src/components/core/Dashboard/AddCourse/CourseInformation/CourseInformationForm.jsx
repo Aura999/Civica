9184f4d4a0b8a7e343f072da4884e8320f9fa17e
Satyam Mishra <satyam2002maanu@gmail.com>
2025-07-23 21:46:54 +0530

    mobile ui dropdown fixed

--- src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -16,7 +16,7 @@ import { setCourse, setStep } from "../../../../../slices/courseSlice"
 import { COURSE_STATUS } from "../../../../../utils/constants"
 import IconBtn from "../../../../Common/IconBtn"
 import Upload from "../Upload"
-import ChipInput from "./ChipInput"
+//import ChipInput from "./ChipInput"
 import RequirementsField from "./RequirementsField"
 
 export default function CourseInformationForm() {
@@ -250,7 +250,7 @@ export default function CourseInformationForm() {
         )}
       </div>
       {/* Course Tags */}
-      <ChipInput
+      {/* <ChipInput
         label="Tags"
         name="courseTags"
         placeholder="Enter Tags and press Enter"
@@ -258,7 +258,7 @@ export default function CourseInformationForm() {
         errors={errors}
         setValue={setValue}
         getValues={getValues}
-      />
+      /> */}
       {/* Course Thumbnail Image */}
       <Upload
         name="courseImage"
