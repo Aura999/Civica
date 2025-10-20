b9d92992e9ff5382a84ba7071303ea521689efc6
Satyam Mishra <satyam2002maanu@gmail.com>
2025-10-21 00:08:15 +0530

    Updated mailSender to use Resend API for OTP emails

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,28 +1,55 @@
-const nodemailer = require("nodemailer")
+// const nodemailer = require("nodemailer")
+
+// const mailSender = async (email, title, body) => {
+//   try {
+//     let transporter = nodemailer.createTransport({
+//       host: process.env.MAIL_HOST,
+//       auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS,
+//       },
+//       secure: false,
+//     })
+
+//     let info = await transporter.sendMail({
+//       from: `"Civica" <${process.env.MAIL_USER}>`, // sender address
+//       to: `${email}`, // list of receivers
+//       subject: `${title}`, // Subject line
+//       html: `${body}`, // html body
+//     })
+//     console.log(info.response)
+//     return info
+//   } catch (error) {
+//     console.log(error.message)
+//     return error.message
+//   }
+// }
+
+// module.exports = mailSender
+
+
+const axios = require("axios");
 
 const mailSender = async (email, title, body) => {
   try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+    const res = await axios.post(
+      "https://api.resend.com/emails",
+      {
+        from: "Civica <satyam2003maanu@gmail.com>",
+        to: `${email}`, // list of receivers
+        subject: `${title}`, // Subject line
+        html: `${body}`, // html body
       },
-      secure: false,
-    })
-
-    let info = await transporter.sendMail({
-      from: `"Civica" <${process.env.MAIL_USER}>`, // sender address
-      to: `${email}`, // list of receivers
-      subject: `${title}`, // Subject line
-      html: `${body}`, // html body
-    })
-    console.log(info.response)
-    return info
-  } catch (error) {
-    console.log(error.message)
-    return error.message
+      {
+        headers: { Authorization: `Bearer ${process.env.RESEND_API_KEY}` },
+      }
+    );
+    console.log("Email API status:", res.status);
+    return res.data;
+  } catch (err) {
+    console.error("Mail API error:", err.message);
+    return null;
   }
-}
+};
 
-module.exports = mailSender
+module.exports = mailSender;
